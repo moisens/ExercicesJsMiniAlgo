04e5d87b971e8df6a69b224d38ba566cfc006ef8
moise <mose.ns@hotmail.com>
2020-07-14 00:29:37 +0200

    Exo 3

---
+++ longestWord.js
@@ -0,0 +1,22 @@
+/*
+Touver le mot le plus long:
+Créer un algorithme qui renvoie la longueur du mot 
+le plus long d'une chaine de caractère.
+*/
+
+const longestWord = (str) =>{
+    let strArr = str.split(' ');
+    let longWord = [];
+    let word = []
+    for(let i=0; i<strArr.length; i++){
+        longWord.push(strArr[i].length)
+        //console.log(longWord);
+    
+    }
+    return `The longetst word has a length of: ${Math.max(...longWord)}`;
+    
+
+    
+
+}
+console.log(longestWord('Understanding and analysis of business needs'))
