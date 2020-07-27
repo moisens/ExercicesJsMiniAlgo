0d327601a86284aadf6579caf2a18f9037942893
moise <mose.ns@hotmail.com>
2020-07-27 23:15:58 +0200

    check End Of Str

--- verifLaFinChaine.js
+++ verifLaFinChaine.js
@@ -1,1 +1,18 @@
-//Tonight......
+function checkEndOfStr(str, target){
+    const endStr = str.split('').slice(-1).join('');
+    console.log(endStr);
+    const lastWord = str.split(' ').slice(-1).join('')
+    console.log(lastWord);
+    const bothChars = str.substr(-target.length);
+    console.log(bothChars);
+    
+    if(endStr === target || lastWord === target || bothChars === target){
+        console.log(`The target MATCHS the last element`);
+    }else{
+        console.log(`The target DOESN'T MATCH the last element`);
+    }
+    
+    
+    
+}
+checkEndOfStr('Hello les gens', 'gens')
